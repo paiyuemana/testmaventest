1c87ad07200434d18872045ff52fe6f2fc0c4b91
mana <na.ma@paiyue.com>
2016-07-26 17:12:28 +0800

    修改main函数

--- src/main/java/testmaventest/path.java
+++ src/main/java/testmaventest/path.java
@@ -4,9 +4,6 @@ import java.io.File;
 
 public class path {
 	public static void main(String args[]){
-		File file=new File("/home/cookieid");
-		File[] files=file.listFiles();
-		System.out.println(files[0].getAbsolutePath());
-		System.out.println(files[0].getParent());
+		
 	}
 }
